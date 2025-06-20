958af63666d2e7fc0a58e720a7efa3631efe400c
Papa Yesh <62230688+Joshua-Dugas@users.noreply.github.com>
2025-06-19 20:23:12 -0500

    Enabled Thymeleaf HTML Rendering
    Added a placeholder homepage and enabled thymeleaf html rendering. Running the app will now render a home page when going to localhost.

---
+++ AbeChatServer/src/main/java/com/abechat/server/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.abechat.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/abechat")
+public class HomeController {
+    /*
+    This is how you render html docs with thymeleaf. 
+    This looks at resources/templates for a html file that matches the returned string.
+    */
+    @GetMapping(path = {"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/test")
+    public String testString(){
+        return "Message Sent Successfully";
+    }
+}
