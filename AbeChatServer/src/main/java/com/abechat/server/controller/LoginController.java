bfc488296d9a31994f6469c6026346af80dc249b
Papa Yesh <62230688+Joshua-Dugas@users.noreply.github.com>
2025-06-23 16:57:47 -0500

    Added page navigation and created a login page/form. Added New styling to make pages look more uniform with each other.

--- AbeChatServer/src/main/java/com/abechat/server/controller/LoginController.java
+++ AbeChatServer/src/main/java/com/abechat/server/controller/LoginController.java
@@ -1,9 +1,16 @@
 package com.abechat.server.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 @RequestMapping("/user/login")
 public class LoginController {
+    //Rendering for login html page
+    @GetMapping(path = {"", "/"})
+    public String login() {
+        return "login";
+    }
 }
