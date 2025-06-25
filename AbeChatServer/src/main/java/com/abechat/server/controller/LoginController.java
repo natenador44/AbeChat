fbfffd4ff84d9dd3e13670b50da513980ba5bbf5
Papa Yesh <62230688+Joshua-Dugas@users.noreply.github.com>
2025-06-25 13:05:11 -0500

    Adjusted login form and pathing to match spring specifications | removed unused import | added error text to login form

--- AbeChatServer/src/main/java/com/abechat/server/controller/LoginController.java
+++ AbeChatServer/src/main/java/com/abechat/server/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/user/login")
+@RequestMapping("/login")
 public class LoginController {
     //Rendering for login html page
     @GetMapping(path = {"", "/"})
