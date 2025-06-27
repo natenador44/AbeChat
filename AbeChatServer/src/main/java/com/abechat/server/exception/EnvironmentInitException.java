87907050f2f406efd77ffa1828474a5bd1f7fa73
natenador44 <nkrolldev@gmail.com>
2025-06-26 22:01:39 -0500

    setup a release profile environment for abe chat using docker compose using an actual postgres database, with connection details supplied via environment variables

--- AbeChatServer/src/main/java/com/abechat/server/exception/EnvironmentInitException.java
+++ AbeChatServer/src/main/java/com/abechat/server/exception/EnvironmentInitException.java
@@ -1,8 +1,18 @@
 package com.abechat.server.exception;
 
+import com.abechat.server.config.ReleaseDataSourceConfig;
+
 public class EnvironmentInitException extends RuntimeException {
     public static EnvironmentInitException noDbUrlSpecified() {
-        return new EnvironmentInitException("DB_URL not specified");
+        return new EnvironmentInitException(ReleaseDataSourceConfig.DB_URL_ENV + " not specified");
+    }
+
+    public static EnvironmentInitException noDbUserSpecified() {
+        return new EnvironmentInitException(ReleaseDataSourceConfig.DB_USER_ENV + " not specified");
+    }
+
+    public static EnvironmentInitException noDbPasswordSpecified() {
+        return new EnvironmentInitException(ReleaseDataSourceConfig.DB_PASSWORD_ENV + " not specified");
     }
 
     private EnvironmentInitException(String message) {
