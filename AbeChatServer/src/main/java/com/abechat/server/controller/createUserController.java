7db06c7455a1196227a1a40a2fdaf76b4eba82cc
Papa Yesh <62230688+Joshua-Dugas@users.noreply.github.com>
2025-07-15 15:54:57 -0500

    Update createUserController.java
    setting up backend connection to form.

--- AbeChatServer/src/main/java/com/abechat/server/controller/createUserController.java
+++ AbeChatServer/src/main/java/com/abechat/server/controller/createUserController.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.abechat.server.service.UserService;
+import com.abechat.server.model.Request;
+import org.springframework.http.MediaType;
 
 @Controller
 @RequestMapping("/createUser")
@@ -20,5 +23,15 @@ public class createUserController {
     @GetMapping(path = {"", "/"})
     public String createUserView() {
         return "createUser"; 
+    }
+//TODO get actual field validation and figure out testing
+    @PostMapping(path = {"", "/"}, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String createUser(Request.NewUser newUser){
+        try {
+            userService.createNew(newUser);
+        } catch (Exception e) {
+            return "redirect:/createUser?error";
+        }
+            return "redirect:/";
     }
 }
